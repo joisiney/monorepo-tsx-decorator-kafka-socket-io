31651ae21a4a65153b68c4fc3778caa700e7587a
Joisiney Leandro <joisiney@gmail.com>
2024-02-09 18:06:27 -0300

    fix: dependence Injection

--- packages/lib-hera/src/decorators/injection/index.factory.ts
+++ packages/lib-hera/src/decorators/injection/index.factory.ts
@@ -6,21 +6,23 @@ export class InjectorFactory {
 
   static resolve<T>(target: Type<T>): T {
     if (!target || !target.name) {
-     console.error('Class not found')
-     return null as T
+      console.error('Class not found')
+      return null as T
     }
     const targetName = Reflect.getMetadata('target-name', target)
+
     if (InjectorFactory.instance.has(targetName)) {
       return InjectorFactory.instance.get(targetName)
     }
     const depsName = Reflect.getMetadata('dep', target)
-
-    const depsInstance: any[] = []
-    InjectorFactory.raw.forEach((value, key) => {
-      if (depsName.includes(key)) {
-        depsInstance.push(InjectorFactory.resolve(value))
+    const depsInstance: any[] = depsName.map((dep: string) => {
+      if (!InjectorFactory.raw.has(dep)) {
+        console.error(`Dependency ${dep} not found`)
+        return null
       }
+      return InjectorFactory.resolve(InjectorFactory.raw.get(dep))
     })
+
     const typeInjection = Reflect.getMetadata('type', target)
 
     // eslint-disable-next-line new-cap
