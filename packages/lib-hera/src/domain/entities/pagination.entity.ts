45a4167e59abbe570d8601fad61f8be7e075930f
Joisiney Leandro <joisiney@gmail.com>
2024-02-09 18:05:10 -0300

    feat: class responsible for render the standardized pagination

--- packages/lib-hera/src/domain/entities/pagination.entity.ts
+++ packages/lib-hera/src/domain/entities/pagination.entity.ts
@@ -11,47 +11,78 @@ interface IPaginationHydrate<T> {
     total: number
 }
 export class PaginationEntity<T> {
-    public data!: T[]
+    private _data!: T[]
     private _pages!: number
-    public _total!: number
-    public skip!: number
-    public page!: number
-    public take!: number
+    private _total!: number
+    private _skip!: number
+    private _page!: number
+    private _take!: number
     constructor(
         props: IPaginationBoot | IPaginationHydrate<T>,
     ) {
         if ('data' in props) {
             this.hydrate(props as IPaginationHydrate<T>)
         } else {
-            this.page = props.page
-            this.take = props.take
+            this._page = props.page
+            this._take = props.take
             this.boot(props as IPaginationBoot)
         }
     }
-    
+
     private boot(props: IPaginationBoot) {
-        this.skip = (props.page - 1) * props.take
+        this._skip = (props.page - 1) * props.take
     }
 
     private hydrate(props: IPaginationHydrate<T>) {
-        this.data = props.data
-        this.page = props.page
-        this.take = props.take
-        this.skip = props.skip
+        this._data = props.data
+        this._page = props.page
+        this._take = props.take
+        this._skip = props.skip
         this._pages = props.pages
         this._total = props.total
+    }
+    set data(value: T[]) {
+        this._data = value
+    }
+
+    get data() {
+        return this._data
+    }
+
+    get skip() {
+        return this._skip
+    }
+
+    get take() {
+        return this._take
+    }
+
+    get page() {
+        return this._page
     }
 
     get pages() {
         return this._pages
     }
 
-    get total() {
+    public get total() {
         return this._total
     }
 
     set total(value: number) {
-        this._pages = Math.ceil(value / this.take)
+        this._pages = Math.ceil(value / this._take)
         this._total = value
+    }
+
+    toJSON() {
+        const result = {
+            page: this._page,
+            take: this._take,
+            skip: this._skip,
+            pages: this._pages,
+            total: this._total,
+            data: this._data
+        }
+        return result;
     }
 }
