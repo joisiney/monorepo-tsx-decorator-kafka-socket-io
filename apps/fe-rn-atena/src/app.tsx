3266483a7086af1cf15ecb2a6a1791ca77cfe36a
Joisiney Leandro <joisiney@gmail.com>
2024-02-10 13:46:02 -0300

    feat: setup tailwindcss@3.3.2 rn

--- apps/fe-rn-atena/src/app.tsx
+++ apps/fe-rn-atena/src/app.tsx
@@ -1,20 +1,13 @@
 import { StatusBar } from 'expo-status-bar'
-import { StyleSheet, Text, View } from 'react-native'
+import { Text, View } from 'react-native'
 
 export function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app</Text>
+    <View className="mt-8 px-2 bg-orange-50">
+      <Text className="text-2xl text-black dark:text-white">
+        Open up App.js to start working on your app
+      </Text>
       <StatusBar style="auto" />
     </View>
   )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-})
---
+++ apps/fe-rn-atena/tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ['./src/app.{js,jsx,ts,tsx}', './src/**/*.{js,jsx,ts,tsx}'],
+  darkMode: false,
+  theme: {
+    extend: {
+      fontFamily: {
+        robotobold: ['Roboto_700Bold'],
+        robotosemibold: ['Roboto_500Medium'],
+        robotoregular: ['Roboto_400Regular'],
+      },
+    },
+  },
+  plugins: [],
+}
