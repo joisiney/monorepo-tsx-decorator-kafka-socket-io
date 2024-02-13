f315202806ff1a477aa98945d2ad5cc6f83a7d5d
Joisiney Leandro <joisiney@gmail.com>
2024-02-13 15:21:12 -0300

    vitest

--- packages/vitest-kairos/fe-rn-atena/src/components/home/header.spec.tsx
+++
@@ -1,9 +0,0 @@
-// import { Header } from '@olympus/fe-rn-atena/src/components/home/header'
-// import { render } from '@testing-library/react-native'
-import { describe, expect, it } from 'vitest'
-describe('@olympus/fe-rn-atena/Component-> <Header/>', () => {
-  it.only('should render', () => {
-    // render(<Header total={0} />)
-    expect(true).toBe(true)
-  })
-})
--- packages/vitest-kairos/utils/mocks/pagination.mock.ts
+++
@@ -1,7 +0,0 @@
-export const PaginationMock = <T>(data: T[], page = 1) => {
-  const perPage = 12
-  const total = data.length
-  const pages = Math.ceil(total / perPage)
-  return { page: page, take: perPage, skip: page, pages, total, data }
-}
-export type IPaginationMock<T> = ReturnType<typeof PaginationMock<T>>
