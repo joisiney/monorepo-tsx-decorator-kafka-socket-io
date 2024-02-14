faf6b0f3c3cf5612e643a639ba791de73738999e
Joisiney Leandro <joisiney@gmail.com>
2024-02-13 22:54:46 -0300

    fix: data-source

---
+++ apps/be-apolo/src/infra/database/typeorm/data-source.ts
@@ -0,0 +1,24 @@
+import { join } from 'path'
+import 'reflect-metadata'
+import { DataSource, DataSourceOptions } from 'typeorm'
+import { SnakeNamingStrategy } from 'typeorm-naming-strategies'
+
+const entity = join(__dirname, '..', 'entities', '*.entity.ts')
+const migration = join(__dirname, '..', 'migrations', '*.ts')
+
+const options: DataSourceOptions = {
+  type: 'mysql',
+  host: process.env.DATABASE_HOST ?? 'localhost',
+  port: +(process.env.DATABASE_PORT ?? 3306),
+  username: process.env.DATABASE_USERNAME ?? 'root',
+  password: process.env.DATABASE_PASSWORD ?? 'root',
+  database: process.env.DATABASE_DATABASE ?? 'teste-db',
+  entities: [entity],
+  migrations: [migration],
+  synchronize: false,
+}
+
+export const dataSource = new DataSource({
+  ...options,
+  namingStrategy: new SnakeNamingStrategy(),
+})
